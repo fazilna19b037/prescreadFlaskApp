cb0d7ffbe744aad0233bb5847275ce281a656973
Fazil P S <62882214+fazilna19b037@users.noreply.github.com>
2022-10-07 22:48:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -204,5 +204,5 @@ app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-
-app.run()
+if __name__ == '__main__':
+    app.run()
